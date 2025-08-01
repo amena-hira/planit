6b5cfd18362d7db04b3715c6fd69c657a81ebeb8
amena-hira <amenahira52@gmail.com>
2025-08-01 16:55:38 +0200

    hello add

--- src/main/java/com/descodeuses/planit/controller/AuthController.java
+++ src/main/java/com/descodeuses/planit/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -82,6 +83,17 @@ public class AuthController {
         }
                 
         
+    }
+
+    @GetMapping("/hello")
+    public String Hello() {
+        // ArrayList<Integer> liste = new ArrayList<>();
+        // liste.add(1);
+        // liste.add(2);
+        // liste.add(3);
+        // liste.get(0);
+
+        return "Hello !";
     }
     
 }
